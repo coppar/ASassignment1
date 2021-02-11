0cacb2d05679b1365ed67d109c6b61cd1238c4f9
coppar <57948186+coppar@users.noreply.github.com>
2021-02-11 15:37:58 +0800

    temp update

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,9 +12,5 @@ namespace WebApplication1
         protected void Application_Start(object sender, EventArgs e)
         {
         }
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            HttpContext.Current.Response.AddHeader("X-Frame-Options", "DENY");
-        }
     }
 }
